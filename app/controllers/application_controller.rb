7ec450cdf5e71eb866d095bf31c340fb93975d15
nyros <manaswini.nyros@gmail.com>
2019-08-01 14:47:54 +0530

    code indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 	def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path
-  end
-  
-  def after_sign_in_path_for(resource_or_scope)
-    admin_restaurants_path
-  end
+        new_admin_session_path
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+        admin_restaurants_path
+    end
 end
