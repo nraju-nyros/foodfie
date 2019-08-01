a0fa54b177010c3ddac267fec376d14e9eb58319
nyros <manaswini.nyros@gmail.com>
2019-08-01 14:37:52 +0530

    code indentaion

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,13 @@
 class Restaurant < ApplicationRecord
+	has_and_belongs_to_many :locations
 	has_and_belongs_to_many :dishes
-    has_and_belongs_to_many :locations
 	has_many :pictures, :as => :imageble, :dependent => :destroy
-	accepts_nested_attributes_for :pictures
 
+	accepts_nested_attributes_for :pictures
 	accepts_nested_attributes_for :locations
 
 	validates_presence_of :name,message:"can't be empty"
-    validates_presence_of :email,message:"can't be empty"
-    validates_presence_of :phone,message:"can't be empty"
+	validates_presence_of :email,message:"can't be empty"
+	validates_presence_of :phone,message:"can't be empty"
 
 end
